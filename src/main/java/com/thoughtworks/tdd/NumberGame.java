c58cd0c2c13fb44ca9121fdedf7aef963698680b
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-12 12:36:54 +0800

    finish part-02 about return_Fizz_when_call_number_given_MultipleOfThree

--- src/main/java/com/thoughtworks/tdd/NumberGame.java
+++ src/main/java/com/thoughtworks/tdd/NumberGame.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tdd;
 
 public class NumberGame {
     public static String number(int i){
+        if(i % 3 == 0){
+            return "Fizz";
+        }
         return i+"";
     }
 }
--- src/test/java/com/thoughtworks/tdd/NumberGameTest.java
+++ src/test/java/com/thoughtworks/tdd/NumberGameTest.java
@@ -7,7 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class NumberGameTest {
 
     @Test
-    public void should_return_number_when_call_number_given_NormalNumberSuchAs_1(){
+    public void should_return_number_when_call_number_given_NormalNumber_SuchAs_1(){
         assertEquals("1",NumberGame.number(1));
+    }
+
+    @Test
+    public void should_return_Fizz_when_call_number_given_MultipleOfThree_SuchAs_3(){
+        assertEquals("Fizz",NumberGame.number(6));
     }
 }
